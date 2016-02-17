cf4f3a504cb02e071a49d42a5df6698c84a36622
brow3924 <brow3924@morris.umn.edu>
2016-02-17 16:18:56 -0500

    made two more tests (figured out isNaN function)

--- client/views/gpaCalculator/gpaCalcMain.controller.spec.js
+++ client/views/gpaCalculator/gpaCalcMain.controller.spec.js
@@ -16,8 +16,19 @@ describe('Testing controller: gpaCalcMainCtrl', function() {
         });
     }));
 
-    it("tesing calculator", function(){
+    it("testing calculator: multiple grades", function(){
         var data = [{"class":"Math","credit":3,"letter":"A","_id":"56c4de359151241b16c9f6b4","__v":0},{"class":"asdf","credit":1,"letter":"B","_id":"56c4de469151241b16c9f6b5","__v":0}];
         expect(gpaCalcMainCtrl.calculateGPA(data)).toEqual(3.75);
     });
+
+    it("testing calculator: one grade", function(){
+        var data = [{"class":"asdf","credit":1,"letter":"B","_id":"56c4de469151241b16c9f6b5","__v":0}];
+        expect(gpaCalcMainCtrl.calculateGPA(data)).toEqual(3.00);
+    });
+
+    it("testing calculator: empty", function(){
+        var data = [];
+        expect(isNaN((gpaCalcMainCtrl.calculateGPA(data)))).toBe(true);
+    });
+
 });
