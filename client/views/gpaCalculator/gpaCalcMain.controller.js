bd52086f3855f7844a7834833362b1d698ecf310
brow3924 <brow3924@morris.umn.edu>
2016-02-17 14:05:19 -0500

    integrated previous lab's gpaCalculator stuff into this one

--- client/views/gpaCalculator/gpaCalcMain.controller.js
+++ client/views/gpaCalculator/gpaCalcMain.controller.js
@@ -4,7 +4,7 @@
 'use strict';
 
 angular.module('appModule')
-    .controller('gpaCalcCtrl', function($http){
+    .controller('gpaCalcMainCtrl', function($http){
         //text fields for class, the class's credits and the class's grade
         var self = this;
 
@@ -14,18 +14,32 @@ angular.module('appModule')
 
         //array where classes are stored
         //initialized as empty
-        self.gpadata = [
+        self.gpadata = [];
 
-        ];
+        self.getCourses = function () {
+            $http.get('api/GPA').success(function (courses) {
+                self.gpadata = courses;
+            });
+        };
+
+        self.getCourses();
 
         //adds classes, credits and grade to the list gpadata so long as all fields have something in them and are not in error
-        self.addClasses = function(){
+        self.addCourses = function(){
             if((self.classNames.length >= 1) && (self.classCredits.length >= 1) && (self.classGrades.length >= 1) && !(isErrorCredits(self.classCredits))) {
-                self.gpadata.push({Class: self.classNames, Credits: self.classCredits, Grade: self.classGrades});
+                $http.post('api/GPA', {class: self.classNames, credit: self.classCredits, letter: self.classGrades}).success(function () {
+                    self.getCourses();
+                });
                 self.classNames = "";
                 self.classCredits = "";
                 self.classGrades = "";
             }
+        };
+
+        self.removeCourses = function (index) {
+            $http.delete('/api/GPA/' + self.gpadata[index]._id).success(function () {
+                self.getCourses();
+            });
         };
 
 
@@ -35,8 +49,8 @@ angular.module('appModule')
             var totalCredits = 0;
             var i = 0;
             for(i = 0; i < self.gpadata.length; i++){
-                qualityPoints = qualityPoints + (parseInt(self.gpadata[i].Credits) * gradeToNumber(self.gpadata[i].Grade));
-                totalCredits = totalCredits + parseInt(self.gpadata[i].Credits);
+                qualityPoints = qualityPoints + (parseInt(self.gpadata[i].credit) * self.gradeToNumber(self.gpadata[i].letter));
+                totalCredits = totalCredits + parseInt(self.gpadata[i].credit);
             }
 
 
@@ -44,17 +58,60 @@ angular.module('appModule')
 
         };
 
+        self.itemsInList = function () {
+            return self.gpadata.length;
+        };
+
         //determines if fields credits and grades are in error, if so, returns an error message to be displayed
         self.returnError = function(){
-            if(isErrorCredits(self.classCredits) && isErrorGrades(self.classGrades)){
+            if(self.isErrorCredits(self.classCredits) && self.isErrorGrades(self.classGrades)){
                 return "Not a number and not a valid grade.";
             }
-            else if(isErrorGrades(self.classGrades)){
+            else if(self.isErrorGrades(self.classGrades)){
                 return "Not a valid grade.";
             }
-            else if(isErrorCredits(self.classCredits)){
+            else if(self.isErrorCredits(self.classCredits)){
                 return "Not a number.";
             }
+        };
+        self.gradeToNumber = function(gr){
+            if(gr == "A")
+            {
+                return 4.0;
+            }
+            if(gr == "B")
+            {
+                return 3.0;
+            }
+            if(gr == "C")
+            {
+                return 2.0;
+            }
+            if(gr == "D")
+            {
+                return 1.0;
+            }
+            if(gr == "F")
+            {
+                return 0.0;
+            }
+
+        };
+
+        self.isErrorCredits = function(cr) {
+            if (Number(cr) == cr) {
+                return false;
+            }
+            return true;
+        };
+
+//determines if grade entry is in error, if it is returns true, if it is not returns false
+        self.isErrorGrades = function(gr) {
+            if ((gr == "A") || (gr == "B") || (gr == "C") || (gr == "D") || (gr == "F") || (gr == "")) {
+                return false;
+
+            }
+            return true;
         };
 
     });
